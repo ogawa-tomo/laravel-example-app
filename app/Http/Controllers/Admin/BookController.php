ae360684d24966e90552ce1eac5860d24d4325e1
tomo <o_tomo_1990@yahoo.co.jp>
2025-04-27 20:53:25 +0900

    書籍登録画面

--- app/Http/Controllers/Admin/BookController.php
+++ app/Http/Controllers/Admin/BookController.php
@@ -3,9 +3,12 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\BookPostRequest;
 use App\Models\Book;
+use App\Models\Category;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class BookController extends Controller
 {
@@ -19,6 +22,25 @@ class BookController extends Controller
     public function show(string $id): Book
     {
         $book = Book::findOrFail($id);
+        return $book;
+    }
+
+    public function create(): View
+    {
+        $categories = Category::all();
+
+        return view('admin/book/create', [
+            'categories' => $categories,
+        ]);
+    }
+
+    public function store(BookPostRequest $request): Book
+    {
+        $book = new Book();
+        $book->category_id = $request->category_id;
+        $book->title = $request->title;
+        $book->price = $request->price;
+        $book->save();
         return $book;
     }
 }
