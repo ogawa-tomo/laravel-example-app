d4b4c0cf15ffa8a9995fda629422b2125c4ae0d1
tomo <o_tomo_1990@yahoo.co.jp>
2025-04-27 21:34:30 +0900

    コンポーネント化

--- resources/views/admin/book/index.blade.php
+++ resources/views/admin/book/index.blade.php
@@ -1,19 +1,13 @@
-<!DOCTYPE html>
-<html lang="ja">
-<head>
-    <meta charset="UTF-8">
-    <title>書籍一覧</title>
-</head>
-<body>
-    <main>
-        <h1>書籍一覧</h1>
-        @if (session('message'))
-            <div style="color:blue">
-                {{ session('message') }}
-            </div>
-        @endif
-        <a href="{{ route('book.create') }}">追加</a>
-        <x-book-table :$books />
-    </main>
-</body>
-</html>
+<x-layouts.book-manager>
+    <x-slot:title>
+        書籍一覧
+    </x-slot:title>
+    <h1>書籍一覧</h1>
+    @if (session('message'))
+        <x-alert class="info">
+            {{ session('message') }}
+        </x-alert>
+    @endif
+    <a href="{{ route('book.create') }}">追加</a>
+    <x-book-table :$books />
+</x-layouts.book-manager>
