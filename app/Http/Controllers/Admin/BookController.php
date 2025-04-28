bb9c81a065f44e16b0384f48e17ab881a37ec689
tomo <o_tomo_1990@yahoo.co.jp>
2025-04-29 08:38:45 +0900

    書籍更新機能

--- app/Http/Controllers/Admin/BookController.php
+++ app/Http/Controllers/Admin/BookController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Http\Requests\BookPostRequest;
+use App\Http\Requests\BookPutRequest;
 use App\Models\Author;
 use App\Models\Book;
 use App\Models\Category;
@@ -46,5 +47,29 @@ class BookController extends Controller
             $book->authors()->attach($request->author_ids);
         });
         return redirect(route('book.index'))->with('message', $book->title . 'を追加しました。');
+    }
+
+    public function edit(Book $book): View
+    {
+        $categories = Category::all();
+        $authors = Author::all();
+
+        $authorIds = $book->authors()->pluck('id')->all();
+
+        return view('admin/book/edit', compact('book', 'categories', 'authors', 'authorIds'));
+    }
+
+    public function update(BookPutRequest $request, Book $book): RedirectResponse
+    {
+        $book->category_id = $request->category_id;
+        $book->title = $request->title;
+        $book->price = $request->price;
+
+        DB::transaction(function() use($book, $request) {
+            $book->update();
+            $book->authors()->sync($request->author_ids);
+        });
+
+        return redirect(route('book.index'))->with('message', $book->title . 'を変更しました。');
     }
 }
--- resources/views/components/book-table.blade.php
+++ resources/views/components/book-table.blade.php
@@ -3,6 +3,7 @@
         <th>カテゴリ</th>
         <th>書籍名</th>
         <th>価格</th>
+        <th>更新</th>
     </tr>
     @foreach($books as $book)
         <tr @if ($loop->even) style="background:#EEE" @endif>
@@ -13,6 +14,11 @@
                 </a>
             </td>
             <td>{{ $book->price }}</td>
+            <td>
+                <a href="{{ route('book.edit', $book) }}">
+                    <button>更新</button>
+                </a>
+            </td>
         </tr>
     @endforeach
 </table>
