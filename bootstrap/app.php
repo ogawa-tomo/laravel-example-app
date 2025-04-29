6686241e64f09d13a0ecbd5619ca3e25b1248509
tomo <o_tomo_1990@yahoo.co.jp>
2025-04-29 11:16:09 +0900

    sqlのログ

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Middleware\AccessLogger;
+use App\Providers\DatabaseServiceProvider;
 use Illuminate\Foundation\Application;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
