c4738ba79cc6880a1772d523863731e3603e0ca6
tomo <o_tomo_1990@yahoo.co.jp>
2025-04-29 19:48:05 +0900

    APIのテスト

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::prefix('messages')
     ->controller(MessageController::class)
-    ->name('api.message')
+    ->name('api.message.')
     ->group(function () {
         Route::get('', 'index')->name('index');
         Route::get('{message}', 'show')->name('show');
